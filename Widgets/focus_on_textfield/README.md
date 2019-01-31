4948f1446f184f1d02030d7dfd4ce040ac600356
sherifhasan <sherif.hasan1994@gmail.com>
2019-01-31 22:32:51 +0200

    Focus on Textfield

---
+++ Widgets/focus_on_textfield/README.md
@@ -0,0 +1,2 @@
+# focus_on_textfield
+
---
+++ Widgets/focus_on_textfield/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Text Field Focus',
+      home: MyCustomForm(),
+    );
+  }
+}
+
+// Define a Custom Form Widget
+class MyCustomForm extends StatefulWidget {
+  @override
+  _MyCustomFormState createState() => _MyCustomFormState();
+}
+
+// Define a corresponding State class. This class will hold the data related to
+// the form.
+class _MyCustomFormState extends State<MyCustomForm> {
+  // Define the focus node. To manage the lifecycle, create the FocusNode in
+  // the initState method, and clean it up in the dispose method
+  FocusNode myFocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+
+    myFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    // Clean up the focus node when the Form is disposed
+    myFocusNode.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Text Field Focus'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // The first text field will be focused as soon as the app starts
+            TextField(
+              autofocus: true,
+            ),
+            // The second text field will be focused when a user taps on the
+            // FloatingActionButton
+            TextField(
+              focusNode: myFocusNode,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        // When the button is pressed, ask Flutter to focus our text field using
+        // myFocusNode.
+        onPressed: () => FocusScope.of(context).requestFocus(myFocusNode),
+        tooltip: 'Focus Second Text Field',
+        child: Icon(Icons.edit),
+      ),
+    );
+  }
+}
